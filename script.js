6e61fb7cfe9c49206573748599c9ce2ddf04aaac
alan <alan@iMac-de-alan.local>
2023-03-06 15:44:23 -0600

    pseudocode for three objects. gameBoard will create dom elements for a 3X3 board and store it into an array. playerFactory will create players and pass a value their value. displayController will print the values into the gameboard and call the results of the match

---
+++ script.js
@@ -0,0 +1,24 @@
+// objeto gameboard
+    // crea un tablero
+    // tiene un metodo para seleccionar casillas con click
+    // exporta las condiciones para ganar
+const gameboard = (() => {
+    const cells = [];
+
+    function createCells (){
+        const
+    }
+})();
+
+
+//objeto displayController
+    // toma el valor de los jugadores y lo pasa al gameboard
+    // tiene las condiciones de turno y reglas
+    //pone en display al ganador
+
+
+//objeto fabrica de jugadores
+    //crea un boton con valor X y otro con valor O que se pueden seleccionar con click
+    // toma el valor del boton y lo pasa a la fabrica de jugadores
+    // tiene un metodo para exportar ese valor con un click
+    // el valor que no se eligio se vuelve el del bot
