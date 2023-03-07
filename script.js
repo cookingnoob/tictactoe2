d72683769741528a10d8bff9ea41dc10e4e86d6f
alan <alan@iMac-de-alan.local>
2023-03-07 12:54:02 -0600

    add div elemts for cells and player selection

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-// objeto gameboard
-    // crea un tablero
-    // tiene un metodo para seleccionar casillas con click
-    // exporta las condiciones para ganar
-const gameboard = (() => {
-    const cells = [];
 
-    function createCells (){
-        const
-    }
+const Gameboard = (() => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => console.log('hola'))
+
+   return {}
+})();
+
+const displayController = (() => {
+
 })();
 
 
-//objeto displayController
-    // toma el valor de los jugadores y lo pasa al gameboard
-    // tiene las condiciones de turno y reglas
-    //pone en display al ganador
+const playerFactory = (value) => {
+
+};
 
 
-//objeto fabrica de jugadores
-    //crea un boton con valor X y otro con valor O que se pueden seleccionar con click
-    // toma el valor del boton y lo pasa a la fabrica de jugadores
-    // tiene un metodo para exportar ese valor con un click
-    // el valor que no se eligio se vuelve el del bot
+//Gameboard:
+ // definir un array vacio
+   // tener una forma de crear un objeto celda y empujarlo al dom
+   // crear multiples celdas
+   // tener una forma de hacer el render en el DOM de esa celdas
+   // event listener para seleccionar cada celda
+   // retorna los valores de las celdas
+   // retorna el event listener
+
+//displayController:
+    // tiene acceso al valor del jugador
+    // accede a gameboard a traves del event listener click
+    // pone el valor del jugador en turno en la celda
+    // checa si alguien gano
+    // se repite hasta que alguien gane o sea un empate
+    // crea en el DOM un letrero del resultado de la partida
+
+
+//playerFacotry:
+    // crea en el DOM un boton con el valor asignado
+    // exporta ese valor a displayController
+    // el valor que no se eligio se va al bot (si tomas X, el bot es O)
+        //el jugador bot escoge una celda de forma aleatorea
