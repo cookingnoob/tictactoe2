14b5b0197e5cf8e35635644b65697c013e5eac02
alan <alan@iMac-de-alan.local>
2024-03-19 14:18:44 -0600

    added dom methods for start game button and player value buttons. Created gameController module to handle the logic of the game -start game, turns and check if someone won

--- script.js
+++ script.js
@@ -1,76 +1,103 @@
+//render methods to get board and buttons into the DOM
 const gameboard = (() => {
-    //render methods to get board into the DOM
-    const renderBoard = () => {
-        const body = document.querySelector('body')
-        const board = document.createElement('div')
-        board.classList.add('board')
-        body.appendChild(board)
-        return {board}
+  const renderBoard = () => {
+    const body = document.querySelector("body");
+    const board = document.createElement("div");
+    board.classList.add("board");
+    body.appendChild(board);
+    return { board };
+  };
+
+  const renderCells = () => {
+    const { board } = renderBoard();
+    const cells = [];
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      board.appendChild(cell);
+      cells.push(cell);
     }
 
-    const renderCells = () => {
-        const {board} = renderBoard()
+    return { cells };
+  };
 
-        const cells = []
-            for(let i = 0; i < 9; i++){
-                const cell = document.createElement('div')
-                cell.classList.add('cell')
-                board.appendChild(cell)
-                cells.push(cell)
-            }
-        
-        return {cells}
-    }
+  const renderStartButton = () => {
+    const body = document.querySelector("body");
+    const startButton = document.createElement('button')
+    startButton.innerText = 'Start Game'
+    body.appendChild(startButton)
+    return startButton
+  }
 
-    const cellsEventListeners = (index) => {
-        console.log(index)
-    }
+  const renderPlayerValueButtons = () => {
+    const body = document.querySelector("body");
+    const playerXBtn = document.createElement('button')
+    const playerYBtn = document.createElement('button')
+    playerXBtn.innerText = 'X'
+    playerYBtn.innerText = 'Y'
+    body.append(playerXBtn, playerYBtn)
+    return {playerXBtn, playerYBtn}
+  }
+  return { renderCells , renderStartButton, renderPlayerValueButtons};
+})();
+gameboard.renderPlayerValueButtons()
 
-    const functionalCell = () => {
-        const {cells} = renderCells()
-        cells.forEach((cell, index) => {
-            cell.addEventListener('click',() => cellsEventListeners(index))
-        })
-    }
+const gameController = (() => {
+  const { cells } = gameboard.renderCells();
 
-    return {functionalCell}
-})()
-gameboard.functionalCell()
+  const handleStartGame = () => {
+    console.log('funciona')
+  }
+
+  const startGame = () => {
+    const startButton = gameboard.renderStartButton()
+    startButton.addEventListener('click', handleStartGame)
+  }
+
+  const winningCombinations = [
+    [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6],
+  ];
+
+  const addValueToCells = (cell, index, value) => {
+    cell.innerHTML = value;
+    console.log(`index: ${index} cell: ${cell.innerHTML}`);
+  };
+
+  const handleClickTurn = (cell, index, value) => {
+    addValueToCells(cell, index, value);
+    //checkWinner
+  };
+
+  //returns the array of cells with a functional eventListener
+  const functionalCell = (value) => {
+    cells.forEach((cell, index) => {
+      cell.addEventListener("click", () => handleClickTurn(cell, index, value));
+    });
+  };
+
+  return { functionalCell, startGame };
+})();
+
+gameController.functionalCell('Prueba');
+gameController.startGame()
+
 
 
 // const Gameboard = (() => {
-//     const gameboard = [];
-//     //pushes the cells to the array
-//     const cells = document.querySelectorAll('.cell');
-//     cells.forEach(cell => gameboard.push(cell));
-    
-    
 
 //     const winConditions = (playerValue) => {
-//         const winningCombinations = [
-//             //rows
-//             [0, 1, 2],
-//             [3, 4, 5],
-//             [6, 7, 8],
-//             //columns  
-//             [0, 3, 6], 
-//             [1, 4, 7], 
-//             [2, 5, 8],
-//             //diagonals
-//             [0, 4, 8], 
-//             [2, 4, 6],
-//         ];
+
 //         for (let i = 0; i < winningCombinations.length; i++){
 
 //             const [a, b, c] = winningCombinations[i];
-        
-//             if (gameboard[a].textContent == gameboard[b].textContent && 
-//                 gameboard[b].textContent == gameboard[c].textContent && 
-//                 gameboard[a].textContent == gameboard[c].textContent && 
+
+//             if (gameboard[a].textContent == gameboard[b].textContent &&
+//                 gameboard[b].textContent == gameboard[c].textContent &&
+//                 gameboard[a].textContent == gameboard[c].textContent &&
 //                 gameboard[a].textContent != ''){
 //                 console.log(`${playerValue} wins`);
 //                 return;
-//             } 
+//             }
 //         }
 //     }
 
@@ -85,33 +112,25 @@ gameboard.functionalCell()
 //    return {winConditions, botTurn}
 // })();
 
-// // 
-
+// //
 
 // const displayController = (() => {
-   
-//     const cells = document.querySelectorAll('.cell');
-//     const chooseCell = () => cells.forEach(cell => clickCell(cell));
 //     const clickCell = (cell) => {cell.addEventListener('click', () => {addText(cell)})};
 //     const addText = (cell) => {
 //         if(cell.textContent !== ''){
 //           return;
-//         } else { 
-//           cell.textContent = playerValue; 
+//         } else {
+//           cell.textContent = playerValue;
 //           Gameboard.botTurn()
 //         }
 //         Gameboard.winConditions(playerValue);
 //       };
-   
-      
+
 //       return {chooseCell};
 // })();
 // displayController.chooseCell()
 
 
-// const domElementsFactory = () => {
-
-// }
 
 // const playerFactory = (name) => {
 //    const choosePlayer = document.createElement('div');
@@ -120,9 +139,8 @@ gameboard.functionalCell()
 //    const playerSelection = document.querySelector('.playerSelection');
 //    playerSelection.appendChild(choosePlayer);
 
-
 //    const click = ()=> choosePlayer.addEventListener('click', value);
-//    const value = () =>{ 
+//    const value = () =>{
 //             playerValue = name;
 //             if (playerValue === 'X'){
 //                 botValue = 'O'
@@ -133,13 +151,8 @@ gameboard.functionalCell()
 //     return{click}
 // };
 
-
 // const playerX = playerFactory('X')
 // playerX.click()
 
 // const playerO = playerFactory('O')
 // playerO.click()
-
-
-
-    
